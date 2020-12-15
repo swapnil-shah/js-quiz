b6a25925f9d461b4cb8c74107ceafed9c7ae8c38
swapnil-shah <swapnilshah.me@gmail.com>
2020-12-15 13:40:28 -0500

    added validation and pop up

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
-/*
-  TODO: 2. Select all elements needed
-    * The form element (has the id `quiz-form`)
-    * The answer inputs (have the class `answer`)
-    * BONUS: The questions (have the class `question-item`)
-    * BONUS: The alert (has the id `alert`)
-*/
+const form = document.querySelector("#quiz-form")
+const answerInputs = document.querySelectorAll(".answer")
+const questionItem = document.querySelectorAll(".question-item")
+const alert = document.querySelector("#alert");
+const inputArray = ['answer-1', 'answer-2', 'answer-3']
 
-// TODO: 3. Create a submit event listener for the form that does the following.
-//    1. Prevent the default behaviour
-//    2. Get all selected answers (use the `checked` property on the input to determine if it is selected or not)
-//    3. Loop through the selected answer to see if they are correct or not (Check the value of the answer to see if it is the string "true")
-//    4. For each correct answer add the class `correct` to the parent with the class `question-item` and remove the class `incorrect`.
-//    5. For each incorrect answer add the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
-//    6. BONUS: Make sure unanswered questions show up as incorrect. The easiest way to do this is to add the incorrect class and removing the correct class from all question items before checking the correct answers
-//    7. BONUS: If all answers are correct show the element with the id `alert` and hide it after one second (look into setTimeout) (use the class active to show the alert and remove the class to hide it)
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  // Checking all required fields
+  let allChecked = 0
+  inputArray.forEach(input => {
+    if (document.querySelector(`input[name="${input}"]:checked`) == null) {
+      document.querySelector(`input[name="${input}"]`).closest('.question-item').classList.add("incorrect");
+      document.querySelector('.all-required ').style.display = "block";
+      allChecked++
+    }
+  })
+  if (allChecked == 0) {
+    document.querySelector('.all-required ').style.display = "none";
+    console.log("all checked")
+    let correctAnswers = 0
+    answerInputs.forEach(function (input) {
+      const parentQuestionItem = input.closest('.question-item')
+      if (input.checked) {
+        if (input.checked.toString() === input.value) {
+          showCorrect(parentQuestionItem)
+          correctAnswers++
+        }
+        else {
+          showInCorrect(parentQuestionItem)
+        }
+      }
+    })
+    if (correctAnswers == inputArray.length) {
+      document.getElementById('alert').style.display = 'block'
+      setTimeout(function () {
+        document.getElementById('alert').style.display = 'none'
+      }, 2000);
+    }
+  }
+})
+function showCorrect(ele, countCorrect) {
+  ele.classList.remove("incorrect")
+  ele.classList.add("correct")
+}
+function showInCorrect(ele) {
+  ele.classList.remove("correct")
+  ele.classList.add("incorrect")
+}
